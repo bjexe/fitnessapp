be1ef89ea554ba9732a5cf279336e6f501607934
bjexe <bensimms@knights.ucf.edu>
2023-01-14 16:08:53 -0500

    update styles of landing page

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -4,14 +4,7 @@ import register from '../services/register'
 import {useNavigate} from 'react-router-dom'
 import {useAuth} from '../context/AuthContext'
 
-export default function LandingPage(){
-
-    // state that holds information about the user when logged in
-    const [user, setUser] = React.useState({
-        token: null,
-        username: null,
-        name: null
-    })
+export default function LandingPage() {
 
     // states for registering and logging in
     const [loginStatus, setLoginStatus] = React.useState(null)
@@ -65,7 +58,7 @@ export default function LandingPage(){
     async function handleRegisterSubmit(event){
         event.preventDefault()
         try {
-            const res = await register(registerFormData)
+            await register(registerFormData)
             setRegisterStatus(true)
         } catch(exception) {
             console.log(JSON.stringify(exception, null, 2))
@@ -92,43 +85,48 @@ export default function LandingPage(){
     return (
         <div className='landing-page'>
             <div className="content">
-                <h1 className='header'>
-                    Welcome to Yacked!  Please create an account below.
-                </h1>
+                <div className="headerContainer">
+                    <h1 className='header'>
+                        Yacked
+                    </h1>
+                </div>
+                <div className="forms">
+                    <form onSubmit={handleLoginSubmit}>
+                        <h1 style={{"font-size": "38px", "color": "#e8e9f3"}}>Log in</h1>
+                        <label style={{"color": "#e8e9f3"}}>
+                            Username:
+                            <input type="text" value={loginFormData.username} onChange={handleLoginChange} name="username"/>
+                        </label>
+                        <label style={{"color": "#e8e9f3"}}>
+                            Password:
+                            <input type="password" value={loginFormData.password} onChange={handleLoginChange} name="password"/>
+                        </label>
+                        <button className='submit-btn'>Log in</button>
+                        {loginStatus === false && <p style={{color: 'red'}}>Invalid credentials</p>}
+                        {loginStatus === true && <p style={{color: 'green'}}>Successfully logged in! Redirecting to homepage...</p>}
+                    </form>
+                <h2 className="loginSignupSeparator">or</h2>
                 <form onSubmit={handleRegisterSubmit}>
-                    <h1>Sign up</h1>
-                    <label>
+                    <h1 style={{"font-size": "38px", "color": "#e8e9f3"}}>Sign up</h1>
+                    <label style={{"color": "#e8e9f3"}}>
                         Username:
                         <input type="text" value={registerFormData.username} onChange={handleRegisterChange} name="username"/>
                     </label>
-                    <label>
+                    <label style={{"color": "#e8e9f3"}}>
                         Email: 
                         <input type="email" value={registerFormData.email} onChange={handleRegisterChange} name="email"/>
                     </label>
-                    <label>
+                    <label style={{"color": "#e8e9f3"}}>
                         Password: 
                         <input type="password" value={registerFormData.password} onChange={handleRegisterChange} name="password"/>
                     </label>
-                    <input type="checkbox" id="emailNotifs" checked={registerFormData.emailNotifs} onChange={handleRegisterChange} name="emailNotifs"/>
-                    <label htmlFor='emailNotifs'>Sign up for email notifications</label>
+                    {/* {<input type="checkbox" id="emailNotifs" checked={registerFormData.emailNotifs} onChange={handleRegisterChange} name="emailNotifs"/>
+                    <label htmlFor='emailNotifs'>Sign up for email notifications</label>} */}
                     <button className='submit-btn'>Sign up</button>
                     {registerStatus === false && <p style={{color: 'red'}}>Error registering. Username may be taken.</p>}
                     {registerStatus === true && <p style={{color: 'green'}}>Successfully signed up!</p>}
                 </form>
-                <form onSubmit={handleLoginSubmit}>
-                    <h1>Log in</h1>
-                    <label>
-                        Username:
-                        <input type="text" value={loginFormData.username} onChange={handleLoginChange} name="username"/>
-                    </label>
-                    <label>
-                        Password:
-                        <input type="password" value={loginFormData.password} onChange={handleLoginChange} name="password"/>
-                    </label>
-                    <button className='submit-btn'>Log in</button>
-                    {loginStatus === false && <p style={{color: 'red'}}>Invalid credentials</p>}
-                    {loginStatus === true && <p style={{color: 'green'}}>Successfully logged in! Redirecting to homepage...</p>}
-                </form>
+                </div>
             </div>
         </div>
     )
